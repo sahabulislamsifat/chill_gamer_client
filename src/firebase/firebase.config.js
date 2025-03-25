8f9e7d71a2c22e302d9282daed70d69715726e37
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-26 00:26:31 +0600

    create .env.local file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,36 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBriy4wjrzbSqb9YkRRlYBe1gTMBD1zFOM",
+//   authDomain: "chill-gamer-2024.firebaseapp.com",
+//   projectId: "chill-gamer-2024",
+//   storageBucket: "chill-gamer-2024.firebasestorage.app",
+//   messagingSenderId: "201173778347",
+//   appId: "1:201173778347:web:ceb70e065949e430329cee",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// export default auth;
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBriy4wjrzbSqb9YkRRlYBe1gTMBD1zFOM",
-  authDomain: "chill-gamer-2024.firebaseapp.com",
-  projectId: "chill-gamer-2024",
-  storageBucket: "chill-gamer-2024.firebasestorage.app",
-  messagingSenderId: "201173778347",
-  appId: "1:201173778347:web:ceb70e065949e430329cee",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
